937239d4f5a16cc6f317e0b761b124edbb49a62f
hassan-niang <hassanniang03@gmail.com>
2024-02-04 18:08:49 -0500

    messages

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Loading npm modules
 require('dotenv').config();
 const APIKEY = process.env.APIKEY
 const OpenAI = require('openai')
@@ -7,6 +8,8 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 
+// Routes 
+// Get request to the ChatGPT API to generate a quiz
 app.get('/api/generatequiz', async (req,res)=>{
     const topic = req.query.topic
     const expertise = req.query.expertise
@@ -14,6 +17,7 @@ app.get('/api/generatequiz', async (req,res)=>{
     const questionStyle = req.query.questionStyle
     
     const completion = await openai.chat.completions.create({
+      //Prompt for the AI to generate a quiz
         messages: [
           {
             role: "system",
@@ -37,20 +41,24 @@ app.get('/api/generatequiz', async (req,res)=>{
             so that the quiz taker knows you really are ${questionStyle}.
             `,
           },
+          //Assistant message to the AI
           { role: "user", content: `Give me the quiz.` },
         ],
         model: "gpt-3.5-turbo-16k-0613",
         response_format: { type: "text" },
         temperature: 1,
       });
+
+      //Creatimg a JSON object with the quiz questions
     res.write(JSON.stringify(
         completion.choices[0].message.content
+        //this array is to filter out the filler text by lines so that we can get the questions
         .split('\n')
         .filter(line=>["1","2","3","4","5","6","7","8","9"].includes(line.charAt(0)))
         ))
     res.end()
 })
-
+// Get request to the ChatGPT API to grade a quiz
 app.get('/api/scorequestion', async (req,res)=>{
     const question = req.query.question
     const answer = req.query.answer
@@ -61,6 +69,7 @@ app.get('/api/scorequestion', async (req,res)=>{
         messages: [
           {
             role: "system",
+            //Prompt for the AI to grade the user's answer
             content: `You are a quiz grader with the personality of ${questionStyle}. 
             You are grading the user's answer to the question: ${question}. 
             AS THE QUIZ GRADER YOU MUST HAVE THE FIRST WORD IN YOUR RESPONSE AS EITHER YES OR NO, indicating if the answer is correct or incorrect. 
@@ -80,6 +89,7 @@ app.get('/api/scorequestion', async (req,res)=>{
         model: "gpt-3.5-turbo-16k-0613",
         response_format: { type: "text" },
       });
+      //Creating a JSON object with the result of the grading. Checks it the first word of the AI's response is "Yes" or "No"
       if (completion.choices[0].message.content.substring(0,3) === "Yes"){
         res.write(JSON.stringify({
             result: "Correct",
@@ -94,5 +104,5 @@ app.get('/api/scorequestion', async (req,res)=>{
 
     res.end()
 })
-
+//Port to listen to
 app.listen(4000)
